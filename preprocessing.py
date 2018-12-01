33a5ba0560c89348617e2ba323780627c55bc130
sanjogjung <gcsanjog@gmail.com>
2018-12-01 19:31:42 +0545

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,7 +7,7 @@ from nltk.corpus import stopwords
 
 def message_process(message):
     nopunc=[char.lower() for char in message if char not in string.punctuation] #removing punctuation
-    nopunc=''.join(nopunc) #joining characters to a form
+    nopunc=''.join(nopunc) #joining characters to form a string
     words=nopunc.split() #splitting string to form a token
     ps=nltk.PorterStemmer() 
     stem_words=[ps.stem(word) for word in words] #stemming tokens
